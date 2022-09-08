26b0e97b9a50ac8bb80a14ca2a37e31e6b325d12
Codewithsplendithsplendid <sylvesterngwuesterngwuabuchi@gmail.com>
2022-09-08 16:28:16 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-int puts("\"Programming is like building a multilingual puzzle\n")
+#include <stdio.h>
+/**
+ *main-prints the phrase "Programming is like building a multilingual puzzule
+ *Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
