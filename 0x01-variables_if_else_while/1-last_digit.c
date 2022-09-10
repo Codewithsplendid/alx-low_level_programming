dfa2191f1ca027214295acdb3b5ae7a62b2afb39
Codewithsplendithsplendid <sylvesterngwuesterngwuabuchi@gmail.com>
2022-09-10 12:46:13 -0700

    C variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * main - entry point
- * description: programming is positive, zero, or negative
- * return always (0)[success]
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	int n;
+	int n, l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-	
+
 	if (l > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
