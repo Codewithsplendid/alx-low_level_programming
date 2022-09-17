aa27062c8b8cae21df3d49ffb6e82034b8ea392f
Codewithsplendithsplendid <sylvesterngwuesterngwuabuchi@gmail.com>
2022-09-16 19:22:53 -0700

    C more functions and c[D

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * print_triangle - 
- * Return: 
+ * print_triangle - Prints a triangle
+ * @size: size of triangle
+ * Return: void
  */
 void print_triangle(int size)
 {
-    int i, j;
-    int k = size;
+	int i, j;
+	int k = size;
 
-    if (size <= 0)
-        _putchar('\n');
-
-    for (i = 0; i < size; i++)
-    {
-        k--;
-        for (j = 0; j < size; j++)
-        {
-            if (j >= k)
-                _putchar('#');
-            else
-                _putchar(' ');
-        }
-        _putchar(10);
-    }
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		k--;
+		for (j = 0; j < size; j++)
+		{
+			if (j >= k)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar(10);
+	}
 }
