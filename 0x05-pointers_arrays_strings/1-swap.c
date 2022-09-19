b4a46b64c9da7bb884909aff343c63790b511093
Codewithsplendithsplendid <sylvesterngwuesterngwuabuchi@gmail.com>
2022-09-19 16:04:46 -0700

    C pointers task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps to integers
+ * @a: integer to be swaped
+ * @b: integer to be swaped
+ * Return: 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
