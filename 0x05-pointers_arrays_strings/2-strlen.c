99c55c69ccde0af6813b2a02b7652cdc69e78a91
Codewithsplendithsplendid <sylvesterngwuesterngwuabuchi@gmail.com>
2022-09-20 08:27:35 -0700

    C pointer task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h> 
+
+/**
+ * _strlen - prints the length of a string
+ * @s: size of string
+ * Return size of string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
