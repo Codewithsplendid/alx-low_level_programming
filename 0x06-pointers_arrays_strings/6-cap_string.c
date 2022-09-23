f14c444de2e8c02f47eb40645390cc5bed3e757a
Codewithsplendithsplendid <sylvesterngwuesterngwuabuchi@gmail.com>
2022-09-23 13:39:19 -0700

    More C pointer tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,21 +3,18 @@
 /**
  * cap_string - capitalizes everey word of a string
  * @s: string to modify
- *
  * Return: the resulting string
  */
 char *cap_string(char *s)
 {
 	int x, y;
-
 	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
-	for (x = 0; s[y] != '\0'; x++)
+	for (x = 0; s[x] != '\0'; x++)
 	{
 		if (x == 0 && s[x] >= 'a' && s[x] <= 'z')
 			s[x] -= 32;
-
 		for (y = 0; y < 13; y++)
 		{
 			if (s[x] == spe[y])
