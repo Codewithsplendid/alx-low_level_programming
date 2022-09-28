ca1713593b90ba94390706f6e952063abcb829ea
Codewithsplendithsplendid <sylvesterngwuesterngwuabuchi@gmail.com>
2022-09-28 15:13:39 -0700

    C recursion task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string
@@ -6,8 +7,8 @@
  */
 void _puts_recursion(char *s)
 {
-	if(*s < 1)
-		return ;
+	if (*s < 1)
+		return;
 	printf("%c", *s);
 	_puts_recursion(s + 1);
 }
