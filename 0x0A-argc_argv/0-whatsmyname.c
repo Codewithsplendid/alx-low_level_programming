3949051623e14584becf95867db5693f91d26d99
Codewithsplendithsplendid <sylvesterngwuesterngwuabuchi@gmail.com>
2022-09-30 15:49:35 -0700

    C argc & argv task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - prints a name
+ * @argc: arrgument count
+ * @argv: arrgument vector
+ * Return: String
+ */
+int main(int argc, char *argv[])
+{
+	_putchar("%s\n", argv[0]);
+}
