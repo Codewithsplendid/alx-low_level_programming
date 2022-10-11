e5c341417b1f90fcb419bbe0fbf6ea4a37cf82a8
Codewithsplendithsplendid <sylvesterngwuesterngwuabuchi@gmail.com>
2022-10-11 10:30:56 -0700

    C -  Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,25 @@
+es (11 sloc)  158 Bytes
+
+#include <stdio.h>
+
+
+
+/**
+
+ * main - prints the name of the file
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+	printf("%s\n", __FILE__);
+
+	return (0);
+
+}
